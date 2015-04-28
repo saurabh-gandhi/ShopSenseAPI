caaf23e9cee2403881814725245bcc6eb2d4cdf4
Saurabh Gandhi <saurabh.gandhi@walchandsangli.ac.in>
2015-04-28 22:50:15 +0530

    Heroku procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn ShopSenseAPI.wsgi --log-file x-
+web: gunicorn -b 0.0.0.0:8000 --pythonpath=./ShopSenseAPI ShopSenseAPI.wsgi --log-file logs-
--- movies/service_apis/userdetail.py
+++ movies/service_apis/userdetail.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from movies.utils.serializers import UserSerializer
 
